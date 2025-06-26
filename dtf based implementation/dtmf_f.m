eb82bae5eacbc8223843f47e86df8a4a3ad0c9b2
emma <eoommaaa@gmail.com>
2025-06-26 14:45:52 -0700

    Update dtmf_f.m

--- dtf based implementation/dtmf_f.m
+++ dtf based implementation/dtmf_f.m
@@ -10,6 +10,6 @@ freqs = [697 770 852 941 1209 1336 1477];    % DTMF
 
 % Determine the corresponding index k that is the closest to each touch-tone frequencies
 for i = 1:length(freqs)
-  [~, k] = min(abs(f - freqs(i)));
-  disp([num2str(freqs(i)) ' Hz is at index k = ' num2str(k)]);
+  [~, k(i)] = min(abs(f - freqs(i)));
+  disp(table(freqs(:), k(:), 'VariableNames', {'Frequency (Hz)', 'Index k'}));
 end
