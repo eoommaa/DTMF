0793c21066c71cc09fed98d59f34c16ae6220656
emma <eoommaaa@gmail.com>
2025-06-25 18:38:48 -0700

    Update dtmf_f.m

--- dtf based implementation/dtmf_f.m
+++ dtf based implementation/dtmf_f.m
@@ -6,7 +6,7 @@ N = 2048;
 fs = 8192;              % Sampling rate
 X = fft(x, N);
 f = (0:N-1) * fs / N;
-freqs = [697 770 852 941 1209 1336 1477];      % DTMF frequencies
+freqs = [697 770 852 941 1209 1336 1477];    % DTMF
 
 % Determine the corresponding index k that is the closest to each touch-tone frequencies
 for i = 1:length(freqs)
